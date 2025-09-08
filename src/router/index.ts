7459f6a84ec25f3ccf821875da878d3470ad583a
疯の葉 <124550277+DearCrazyLeaf@users.noreply.github.com>
2025-09-08 08:56:53 +0800

    dev.

--- src/router/index.ts
+++ src/router/index.ts
@@ -140,14 +140,15 @@ const router = createRouter({
       component: () => import('../views/ActivityView.vue'),
       meta: { requiresAuth: true }, // 需要登录
     },
-    {
-      path: '/activity-test',
-      name: 'activity-test',
-      // route level code-splitting
-      // this generates a separate chunk for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ActivityTestView.vue'),
-    },
+    // 临时移除不存在的ActivityTestView路由
+    // {
+    //   path: '/activity-test',
+    //   name: 'activity-test',
+    //   // route level code-splitting
+    //   // this generates a separate chunk for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/ActivityTestView.vue'),
+    // },
   ],
 })
 
