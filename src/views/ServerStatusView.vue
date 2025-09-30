f96048e89d1a4cb60be2c37ad39d41ab87435932
疯の葉 <124550277+DearCrazyLeaf@users.noreply.github.com>
2025-10-01 02:08:23 +0800

    update activity server

--- src/views/ServerStatusView.vue
+++ src/views/ServerStatusView.vue
@@ -152,7 +152,7 @@ import ServerPreviewCard from '@/components/ServerStatus/ServerPreviewCard.vue'
 interface ServerConfig { name: string; address: string }
 const serverConfigs: ServerConfig[] = [
   { name: '娱乐对抗正式服', address: '110.42.41.225:27015' },
-  { name: '活动专用服务器 - 测试中', address: '' }
+  { name: '活动专用服务器 - 测试中', address: '115.231.176.88:27015' }
 ]
 const serverAddresses = serverConfigs.map(c => c.address)
 
