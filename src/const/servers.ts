63bb91e20fad3fb74f73f313428dabfad9ec6b3b
疯の葉 <124550277+DearCrazyLeaf@users.noreply.github.com>
2026-04-08 08:14:44 +0800

    add: l4d2 server card

--- src/const/servers.ts
+++ src/const/servers.ts
@@ -4,7 +4,8 @@ export interface ServerConfig {
 }
 
 export const serverConfigs: ServerConfig[] = [
-  { name: 'CS2-娱乐对抗', address: 'hlymmg.cn:27015' },
+  { name: 'CS2-娱乐对抗', address: 'hlymmg.cn:27015'},
+  { name: 'L4D2-三方多特', address: 'mc.cu.mujica.cloud:40006'},
 ]
 
 export const serverAddresses = serverConfigs.map((config) => config.address ?? '')
